33762ad40b0c2c986946fd9d8a7831e243ad324c
Ramadan <ramadan260906@gmail.com>
2025-01-24 07:07:29 +0700

    model

---
+++ src/model/Petugas.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Hp
+ */
+public class Petugas {
+    private String idPetugas;    
+    private String nmPetugas;
+    private String username;
+    private String password;
+    private String kelas;
+    private String level;
+
+    public String getIdPetugas() {
+        return idPetugas;
+    }
+
+    public void setIdPetugas(String idPetugas) {
+        this.idPetugas = idPetugas;
+    }
+
+    public String getNmPetugas() {
+        return nmPetugas;
+    }
+
+    public void setNmPetugas(String nmPetugas) {
+        this.nmPetugas = nmPetugas;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    
+}
