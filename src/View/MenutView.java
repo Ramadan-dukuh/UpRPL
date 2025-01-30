5373692de4bfef0d349abe6a586e719735cffbba
DaigoX <dafaal171717@gmail.com>
2025-01-30 21:42:00 +0700

    menuDashboard, dan Menut

--- src/View/MenutView.java
+++ src/View/MenutView.java
@@ -14,9 +14,7 @@ import javax.swing.ImageIcon;
  */
 public class MenutView extends javax.swing.JFrame {
 
-    /**
-     * Creates new form MenutView
-     */
+    int xx, xy;
     public MenutView() {
         initComponents();
     }
@@ -34,16 +32,27 @@ public class MenutView extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        pn_main = new javax.swing.JPanel();
-        pn_line = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
+        pn_DashboardMain = new javax.swing.JPanel();
+        pn_DashboardLine = new javax.swing.JPanel();
+        lb_iconDashboard = new javax.swing.JLabel();
+        lb_Dashboard = new javax.swing.JLabel();
         pn_kanan = new java.awt.Panel();
         pn_dasar = new javax.swing.JPanel();
         pn_utama = new javax.swing.JPanel();
         jPanelGradient1 = new pallet.JPanelGradient();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+        addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                formMouseDragged(evt);
+            }
+        });
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                formMousePressed(evt);
+            }
+        });
 
         pn_kiri.setPreferredSize(new java.awt.Dimension(300, 636));
 
@@ -57,58 +66,65 @@ public class MenutView extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(153, 153, 153));
         jLabel2.setText("Master Data");
 
-        pn_line.setBackground(new java.awt.Color(51, 51, 51));
-
-        javax.swing.GroupLayout pn_lineLayout = new javax.swing.GroupLayout(pn_line);
-        pn_line.setLayout(pn_lineLayout);
-        pn_lineLayout.setHorizontalGroup(
-            pn_lineLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 10, Short.MAX_VALUE)
-        );
-        pn_lineLayout.setVerticalGroup(
-            pn_lineLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-        );
-
-        jLabel4.setFont(new java.awt.Font("Verdana", 0, 15)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(153, 153, 153));
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icDashboard.png"))); // NOI18N
-
-        jLabel5.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel5.setText("Dashboard");
-        jLabel5.addMouseListener(new java.awt.event.MouseAdapter() {
+        pn_DashboardMain.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                pn_DashboardMainMouseClicked(evt);
+            }
             public void mouseEntered(java.awt.event.MouseEvent evt) {
-                jLabel5MouseEntered(evt);
+                pn_DashboardMainMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                pn_DashboardMainMouseExited(evt);
             }
         });
 
-        javax.swing.GroupLayout pn_mainLayout = new javax.swing.GroupLayout(pn_main);
-        pn_main.setLayout(pn_mainLayout);
-        pn_mainLayout.setHorizontalGroup(
-            pn_mainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pn_mainLayout.createSequentialGroup()
+        pn_DashboardLine.setBackground(new java.awt.Color(51, 51, 51));
+
+        javax.swing.GroupLayout pn_DashboardLineLayout = new javax.swing.GroupLayout(pn_DashboardLine);
+        pn_DashboardLine.setLayout(pn_DashboardLineLayout);
+        pn_DashboardLineLayout.setHorizontalGroup(
+            pn_DashboardLineLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 10, Short.MAX_VALUE)
+        );
+        pn_DashboardLineLayout.setVerticalGroup(
+            pn_DashboardLineLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        lb_iconDashboard.setFont(new java.awt.Font("Verdana", 0, 15)); // NOI18N
+        lb_iconDashboard.setForeground(new java.awt.Color(153, 153, 153));
+        lb_iconDashboard.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icDashboard.png"))); // NOI18N
+
+        lb_Dashboard.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        lb_Dashboard.setForeground(new java.awt.Color(51, 51, 51));
+        lb_Dashboard.setText("Dashboard");
+
+        javax.swing.GroupLayout pn_DashboardMainLayout = new javax.swing.GroupLayout(pn_DashboardMain);
+        pn_DashboardMain.setLayout(pn_DashboardMainLayout);
+        pn_DashboardMainLayout.setHorizontalGroup(
+            pn_DashboardMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pn_DashboardMainLayout.createSequentialGroup()
                 .addGap(5, 5, 5)
-                .addComponent(pn_line, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(pn_DashboardLine, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(lb_iconDashboard, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel5)
+                .addComponent(lb_Dashboard)
                 .addContainerGap(73, Short.MAX_VALUE))
         );
-        pn_mainLayout.setVerticalGroup(
-            pn_mainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pn_mainLayout.createSequentialGroup()
+        pn_DashboardMainLayout.setVerticalGroup(
+            pn_DashboardMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pn_DashboardMainLayout.createSequentialGroup()
                 .addGap(5, 5, 5)
-                .addComponent(pn_line, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(pn_DashboardLine, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(5, 5, 5))
-            .addGroup(pn_mainLayout.createSequentialGroup()
+            .addGroup(pn_DashboardMainLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(pn_mainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(pn_mainLayout.createSequentialGroup()
+                .addGroup(pn_DashboardMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lb_iconDashboard, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(pn_DashboardMainLayout.createSequentialGroup()
                         .addGap(8, 8, 8)
-                        .addComponent(jLabel5)
+                        .addComponent(lb_Dashboard)
                         .addGap(0, 8, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -128,7 +144,7 @@ public class MenutView extends javax.swing.JFrame {
                         .addGap(10, 10, 10)
                         .addGroup(pn_kiriLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
-                            .addComponent(pn_main, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(pn_DashboardMain, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(35, Short.MAX_VALUE))
         );
         pn_kiriLayout.setVerticalGroup(
@@ -144,8 +160,8 @@ public class MenutView extends javax.swing.JFrame {
                         .addGap(24, 24, 24)))
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(pn_main, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(458, Short.MAX_VALUE))
+                .addComponent(pn_DashboardMain, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(670, Short.MAX_VALUE))
         );
 
         getContentPane().add(pn_kiri, java.awt.BorderLayout.LINE_START);
@@ -153,17 +169,7 @@ public class MenutView extends javax.swing.JFrame {
         pn_kanan.setLayout(new java.awt.BorderLayout());
 
         pn_utama.setBackground(new java.awt.Color(255, 255, 255));
-
-        javax.swing.GroupLayout pn_utamaLayout = new javax.swing.GroupLayout(pn_utama);
-        pn_utama.setLayout(pn_utamaLayout);
-        pn_utamaLayout.setHorizontalGroup(
-            pn_utamaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 884, Short.MAX_VALUE)
-        );
-        pn_utamaLayout.setVerticalGroup(
-            pn_utamaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-        );
+        pn_utama.setLayout(new java.awt.BorderLayout());
 
         jPanelGradient1.setColorEnd(new java.awt.Color(51, 255, 255));
         jPanelGradient1.setColorStart(new java.awt.Color(0, 0, 255));
@@ -172,7 +178,7 @@ public class MenutView extends javax.swing.JFrame {
         jPanelGradient1.setLayout(jPanelGradient1Layout);
         jPanelGradient1Layout.setHorizontalGroup(
             jPanelGradient1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 924, Short.MAX_VALUE)
+            .addGap(0, 1078, Short.MAX_VALUE)
         );
         jPanelGradient1Layout.setVerticalGroup(
             jPanelGradient1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -185,17 +191,17 @@ public class MenutView extends javax.swing.JFrame {
             pn_dasarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanelGradient1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(pn_dasarLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(pn_utama, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(20, 20, 20)
+                .addComponent(pn_utama, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(20, 20, 20))
         );
         pn_dasarLayout.setVerticalGroup(
             pn_dasarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(pn_dasarLayout.createSequentialGroup()
                 .addComponent(jPanelGradient1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(pn_utama, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(22, 22, 22))
+                .addGap(20, 20, 20)
+                .addComponent(pn_utama, javax.swing.GroupLayout.DEFAULT_SIZE, 744, Short.MAX_VALUE)
+                .addGap(20, 20, 20))
         );
 
         pn_kanan.add(pn_dasar, java.awt.BorderLayout.CENTER);
@@ -203,12 +209,39 @@ public class MenutView extends javax.swing.JFrame {
         getContentPane().add(pn_kanan, java.awt.BorderLayout.CENTER);
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jLabel5MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel5MouseEntered
-        pn_main.setBackground(new Color(255,255,255));
-        pn_line.setBackground(new Color(0,255,255));
-    }//GEN-LAST:event_jLabel5MouseEntered
+    private void formMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMousePressed
+        xx = evt.getX();
+        xy = evt.getY();
+    }//GEN-LAST:event_formMousePressed
+
+    private void formMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseDragged
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        this.setLocation(x - xx, y - xy);
+    }//GEN-LAST:event_formMouseDragged
+
+    private void pn_DashboardMainMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pn_DashboardMainMouseClicked
+        pn_DashboardMain.setBackground(new Color(240, 240, 240));
+        pn_DashboardLine.setBackground(new Color(255, 255, 255));
+
+        pn_utama.removeAll();
+        pn_utama.add(new MenuDashboard());
+        pn_utama.repaint();
+        pn_utama.revalidate();
+    }//GEN-LAST:event_pn_DashboardMainMouseClicked
+
+    private void pn_DashboardMainMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pn_DashboardMainMouseEntered
+        pn_DashboardMain.setBackground(new Color(255, 255, 255));
+        pn_DashboardLine.setBackground(new Color(0, 255, 255));
+    }//GEN-LAST:event_pn_DashboardMainMouseEntered
+
+    private void pn_DashboardMainMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pn_DashboardMainMouseExited
+        pn_DashboardMain.setBackground(new Color(240, 240, 240));
+        pn_DashboardLine.setBackground(new Color(51, 51, 51));
+    }//GEN-LAST:event_pn_DashboardMainMouseExited
 
     /**
      * @param args the command line arguments
@@ -249,14 +282,14 @@ public class MenutView extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private pallet.JPanelGradient jPanelGradient1;
+    private javax.swing.JLabel lb_Dashboard;
+    private javax.swing.JLabel lb_iconDashboard;
+    private javax.swing.JPanel pn_DashboardLine;
+    private javax.swing.JPanel pn_DashboardMain;
     private javax.swing.JPanel pn_dasar;
     private java.awt.Panel pn_kanan;
     private java.awt.Panel pn_kiri;
-    private javax.swing.JPanel pn_line;
-    private javax.swing.JPanel pn_main;
     private javax.swing.JPanel pn_utama;
     // End of variables declaration//GEN-END:variables
 }
