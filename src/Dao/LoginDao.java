2e8da24eb34867acdea58f14db313e667bc9a94e
Ramadan <ramadan260906@gmail.com>
2025-01-31 08:09:00 +0700

    Fix Login

--- src/Dao/LoginDao.java
+++ src/Dao/LoginDao.java
@@ -1,15 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Dao;
 
-/**
- *
- * @author Daffa
- * adel
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import static koneksi.Koneksi.getConnection;
+import model.Petugas;
+
 public class LoginDao {
+    Connection kon;
+    PreparedStatement ps;
+    ResultSet rs;
     
+    public LoginDao(){
+        kon = getConnection();
+    }
+    
+    public Petugas getPetugas(String username) {
+        Petugas petugas = null;
+        try {
+            ps = kon.prepareStatement("SELECT idPetugas, username, password FROM petugas WHERE username = ?");
+            ps.setString(1, username);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                petugas = new Petugas();
+                petugas.setIdPetugas(rs.getString("idPetugas"));
+                petugas.setUsername(rs.getString("username"));
+                petugas.setPassword(rs.getString("password"));
+            }
+        } catch (SQLException se) {
+            System.out.println("Error : " + se);
+        }
+        return petugas;
+    }
 }
